81d85b00bfac5abf16a43ea25da3a17957c16ccb
LJH <2509739433@qq.com>
2024-05-17 12:17:07 +0800

    配置管理端请求头authToken

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const service = axios.create({
   baseURL: 'http://localhost:8080/admin',
-  timeout: 600000
+  timeout: 100000
 })
 
 // 请求拦截器：在发送请求之前进行处理
@@ -10,10 +10,17 @@ service.interceptors.request.use(config => {
   // 将请求数据转换为 JSON 格式
   config.headers['Content-Type'] = 'application/json'
   config.data = JSON.stringify(config.data)
+
+  // 配置token
+  const authToken = localStorage.getItem('authToken')
+  console.log('token' + authToken)
+  if (authToken) {
+    config.headers.authToken = authToken
+  }
+
   return config
 }, error => {
   // 处理请求错误
-  console.error('request 请求失败:', error)
   return Promise.reject(error)
 })
 
